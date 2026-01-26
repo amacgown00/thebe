6ce550f8d31470468a09d52a2135be7d32d82d5e
amacgown <amacgown00@gmail.com>
2026-01-26 15:57:34 -0500

    connecting python to csv

--- thebe/metis.py
+++ thebe/metis.py
@@ -1,2 +1,7 @@
-# Trending SFLMS reports
+# Trending SFLMS report
 
+import pandas as pd
+df=pd.read_csv("C:/Users/andrea.macgown/OneDrive - Thermo Fisher Scientific/Desktop/2026/thebe/Query 26Jan2026.csv", 
+encoding="utf-16")
+print("The dataframe is:")
+print(df)
