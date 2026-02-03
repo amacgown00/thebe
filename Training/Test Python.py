302986cb8d147d61deafe32b9da5c966aabb0fe2
amacgown <amacgown00@gmail.com>
2026-02-03 12:16:02 -0500

    Updating Testing Excel python file

--- Training/Test Python.py
+++ Training/Test Python.py
@@ -5,3 +5,11 @@ excel_file = pd.ExcelFile(file_path)
 print("Sheet names: ")
 for index, sheet in enumerate(excel_file.sheet_names):
     print(index, sheet)
+
+print('\n')
+print('This should be row 6')
+row = pd.read_excel(excel_file, sheet_name = 6, usecols=['PR ID']).iloc[0:15]
+print(row)
+
+
+
